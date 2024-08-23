d9432725b9794ee49192e739f027a19dd9c2997f
dcartist <pbannerman13@gmail.com>
2024-08-22 21:05:21 -0400

    adding home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Home from './pages/Home';
 import NavBar from "./components/Navigation/NavBar";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         </Routes>
      
