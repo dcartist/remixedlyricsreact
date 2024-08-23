8b6853896ffe40484487fdb6d52393d19bafb811
dcartist <pbannerman13@gmail.com>
2024-08-23 18:45:11 -0400

    updating song list

--- src/pages/SongList.js
+++ src/pages/SongList.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { AllSongsApi } from '../api/ApiRouters'
 import { PageContainer } from '../components/PageContainer/PageContainer'
 import { Link } from 'react-router-dom';
+import Rating from '../components/Rating/Rating';
 export default function SongList() {
     const SongList = AllSongsApi();
-
+  console.log(SongList)
   return (
     <PageContainer title="Songs" rowClassName="text-left">
-    <ul>
-        {SongList.map((song, index) => <li key={index}><Link to={`/songs/${song.id}`}>{song.title}</Link></li>)}
+    <ul className='list-group'>
+        {SongList.map((song, index) => <li key={index} className="list-group-item list-group-item-action"><Link to={`/songs/${song.id}`} className="d-flex justify-content-between align-items-center">{song.title} { " "} {song.remix_level && <Rating ratingNumber={song.remix_level.level}/>} </Link> </li>)}
     </ul>
     </PageContainer>
   )
